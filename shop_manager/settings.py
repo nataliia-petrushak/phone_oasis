53fde6e76b5bc1ef59d482fc9bdba0bbb373ff46
MrYuriy <yuriybabiyk@gmail.com>
2024-02-12 20:43:03 +0100

    Keep SECRET_KEY in .env file

--- shop_manager/settings.py
+++ shop_manager/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+from datetime import timedelta
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w!1(lryhre&*75)t-lekxlxy=2cw-lq28bq(sf*0w$9cq28z3p"
-
+SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
