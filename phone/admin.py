f39d5573785f203575854ed069ac49442b366b82
Amirammi <amirammina@gmail.com>
2024-02-13 16:08:12 +0200

    Fixed model Phone

--- phone/admin.py
+++ phone/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Phone, Image
 
-# Register your models here.
+
+admin.site.register(Image)
+
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "screen",
+        "resolution",
+        "processor",
+        "camera",
+        "zoom",
+        "cell",
+        "about",
+    )
--- phone/models.py
+++ phone/models.py
@@ -1,35 +1,30 @@
+import os
+import uuid
+
 from django.db import models
 
-
-class Memory(models.Model):
-    ram = models.CharField(max_length=255)
-    capacity = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+from device.models import Device
+from django.utils.text import slugify
 
 
-class Color(models.Model):
-    name = models.CharField(max_length=255)
-    hex_value = models.CharField(max_length=255, blank=True, null=True)
+class Phone(Device):
+    cell = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+def phone_image_file_path(instance, filename: str):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads", "phones", filename)
 
 
 class Image(models.Model):
-    url = models.URLField()
-
-
-class Phone(models.Model):
     name = models.CharField(max_length=255)
-    memories = models.ManyToManyField(Memory, related_name="phones")
-    colors = models.ManyToManyField(Color, related_name="phones")
-    screen = models.CharField(max_length=255)
-    resolution = models.CharField(max_length=255)
-    processor = models.CharField(max_length=255)
-    camera = models.CharField(max_length=255)
-    zoom = models.CharField(max_length=255)
-    cell = models.CharField(max_length=255)
-    images = models.ForeignKey(Image, on_delete=models.CASCADE)
-    about = models.TextField()
-    is_favourite = models.BooleanField(default=False)
-    in_cart = models.BooleanField(default=False)
+    phones = models.ForeignKey(Phone, on_delete=models.CASCADE, related_name="images", null=True)
+    url = models.ImageField(upload_to=phone_image_file_path, null=True)
 
     def __str__(self) -> str:
         return self.name
