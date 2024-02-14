b960f8d00b0b79fe129019a8b6ce54d7239489de
Amirammi <amirammina@gmail.com>
2024-02-14 18:08:10 +0200

    Initialized new app tablet

--- shop_manager/settings.py
+++ shop_manager/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "device",
-    "phone"
+    "phone",
+    "tablet"
 ]
 
 MIDDLEWARE = [
--- shop_manager/urls.py
+++ shop_manager/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/phones/", include("phone.urls", namespace="phone"))
+    path("api/phones/", include("phone.urls", namespace="phone")),
+    path("api/tablets/", include("tablet.urls", namespace="tablet"))
 ]
